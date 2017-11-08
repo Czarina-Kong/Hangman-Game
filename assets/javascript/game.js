068168f6a28d475d7fa71b25ebea43f8cdeebdca
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2017-11-07 23:14:37 -0800

    pull random word from words array for user to guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//let's create an array of words
+//create an array of words
 
 var words = ['sushi', 'oatmeal', 'scallops', 'venison', 'adobo',
 	 'horseradish', 'dumpling', 'guavas', 'potato', 'cookies', 
@@ -12,3 +12,10 @@ var words = ['sushi', 'oatmeal', 'scallops', 'venison', 'adobo',
 // for (var i = 0; i < words.length; i++) {
 // 	console.log(words[i])
 // }
+
+//begin game when any key is pressed
+//choose random word and display _ for every letter
+
+// document.onkeyup = function(){
+	var randomWord = words[Math.floor(Math.random() * words.length)];
+	document.getElementById('game').innerHTML = randomWord;
