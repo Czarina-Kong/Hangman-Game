e8077b5fb12ff6038eafc63db901737e6803a190
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2017-11-07 22:46:11 -0800

    create words array in js

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//let's create an array of words
+
+var words = ['sushi', 'oatmeal', 'scallops', 'venison', 'adobo',
+	 'horseradish', 'dumpling', 'guavas', 'potato', 'cookies', 
+	 'custard', 'tortillas', 'avocados', 'steak', 'breadcrumbs', 
+	 'pepper', 'ketchup', 'artichoke', 'molasses', 'mint',
+	 'arugula', 'almonds', 'sardines', 'rosemary', 'granola',
+	 'honey', 'buckwheat', 'tarragon', 'coriander', 'cucumber',
+	 'beer', 'cheddar', 'walnuts', 'vinegar', 'yogurt'];
+
+//make sure array works
+// for (var i = 0; i < words.length; i++) {
+// 	console.log(words[i])
+// }
